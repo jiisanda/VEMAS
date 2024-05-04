3e4676e5fd3e9b2d1525328ce931ddb61b8e47fe
jiisanda <jiisanda@protonmail.com>
2024-05-04 17:09:29 +0530

    minor vendor model change

--- vendor/admin.py
+++ vendor/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from vendor.models import Vendor, PerformanceHistory
 
+
+class VendorAdmin(admin.ModelAdmin):
+    readonly_fields = ('on_time_delivery_rate', 'average_response_time', 'quality_rating_avg', 'fulfillment_rate')
+
+
 # Register vendor models here
-admin.site.register(Vendor)
+admin.site.register(Vendor, VendorAdmin)
 admin.site.register(PerformanceHistory)
--- vendor/models.py
+++ vendor/models.py
@@ -11,10 +11,10 @@ class Vendor(models.Model):
     name = models.CharField(max_length=100, unique=True)
     contact_details = models.TextField()
     address = models.TextField()
-    on_time_delivery_rate = models.FloatField(default=0.0)
-    quality_rating_avg = models.FloatField(default=0.0)
-    average_response_time = models.FloatField(default=0.0)
-    fulfillment_rate = models.FloatField(default=0.0)
+    on_time_delivery_rate = models.FloatField(default=0.0, editable=False)
+    quality_rating_avg = models.FloatField(default=0.0, editable=False)
+    average_response_time = models.CharField(max_length=100, editable=False, blank=True)
+    fulfillment_rate = models.FloatField(default=0.0, editable=False)
 
     def save(self, *args, **kwargs):
         if not self.uvc:
